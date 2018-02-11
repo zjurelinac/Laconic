e96f11fbfbb94b63a2f84ddbde5c631ab7a76bf7
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-02-11 20:57:37 +0100

    Implemented Dispatchable and DispatchParam; extended AttributeScope with .all() method

--- laconic/util.py
+++ laconic/util.py
@@ -7,13 +7,8 @@ Classes:
     Config - Dict-like object for storing app configuration
     Namespace - Simple namespace for storing arbitrary data.
     AttributeScope - Dict which allows searching for keys in parent scopes
-    NestedList -
-
-    # ImmutableDict - ?
-    # CombinedDict - ?
-    # SortedList - ?
-    # SortedPriorityList - ?
-
+    ImmutableOrderedList -
+    Dispatchable -
 
 Functions:
     url_join - Join a sequence of URLs together in a single hierarchical URL
@@ -26,12 +21,21 @@ License:    MIT, see LICENSE for more details
 """
 
 # import bisect
-# import json
-# import re
 
-import collections.abc
+import functools
+import operator
 import os
 import types
+
+from collections.abc import Callable, MutableMapping, Sequence
+from inspect import signature, Signature
+
+
+# Generic sentinel value representing missing parameter/attribute
+_MISSING = object()
+
+
+# Generic datastructures
 
 class Config(dict):
     """Simple dictionary storing app configuration.
@@ -51,7 +55,7 @@ class Config(dict):
     def __missing__(self, key):
         return None
 
-    def from_pyfile(self, filename: str):
+    def from_pyfile(self, filename: str) -> None:
         """Load configuration parameters from a Python file.
 
         Args:
@@ -72,7 +76,7 @@ class Config(dict):
             raise
         self.from_object(d)
 
-    def from_object(self, obj: object):
+    def from_object(self, obj: object) -> None:
         """Load configuration parameters from an object.
 
         Args:
@@ -101,12 +105,17 @@ class Namespace:
         return self.__dict__ == other.__dict__
 
 
-class AttributeScope(collections.abc.MutableMapping):
+class AttributeScope(MutableMapping):
     """Nested attribute scope which allows searching for keys in parent scopes.
 
     Provides a dict-like interface, with the difference that a key missing in
     this scope can be retrieved from any of the parent scopes (if it's there).
+
+    Methods (additional):
+        all - Return all values this key maps to from the whole scope hierarchy
     """
+
+    __slots__ = ['_data', '_parent']
 
     def __init__(self, _parent=None, **items):
         self._data = items
@@ -135,6 +144,19 @@ class AttributeScope(collections.abc.MutableMapping):
     def __repr__(self):
         return '<%s %s, parent=%s>' % (self.__class__.__name__, self._data, self._parent)
 
+    def all(self, key) -> list:
+        """Return all values this key maps to from the whole scope hierarchy
+
+        Returns a list of all values corresponding to the given key in the whole
+        scope hierarchy, ordered bottop-up
+        """
+        scope, result = self, []
+        while scope is not None:
+            if key in scope:
+                result.append(scope[key])
+            scope = scope._parent
+        return result
+
     def _combine(self):
         ds, t = [], self
         while t is not None:
@@ -148,21 +170,116 @@ class AttributeScope(collections.abc.MutableMapping):
         return rd
 
 
-# class SortedList:
-#     # def __init__(self, *args):
-#     #     self._items = sorted(args)
+class ImmutableOrderedList(Sequence):
+    """Immutable ordered list of objects of any kind."""
 
-#     # def add(self, item):
-#     #     """Add an element to the sorted list, which remains sorted afterwards"""
-#     #     bisect.insort(self._items, item)
+    __slots__ = ['_items']
 
-#     # def remove(self, item):
-#     #     """Remove an element from the sorted list"""
-#     #     self._items.remove(item)
+    def __init__(self, iterable, cmp=None, reverse=False):
+        """ImmutableOrderedList constructor.
 
-#     # def __repr__(self):
-#     #     return 'SortedList(%s)' % (', '.join(repr(i) for i in self._items))
-#     pass
+        Creates an immutable ordered list from the given iterable. Ordering can
+        be performed either with default or with a custom comparator, and either
+        in normal or reversed order.
+
+        Args:
+            iterable (Iterable): Iterable sequence of items to build the
+                immutable ordered list from
+            cmp (optional, Callable): Custom comparator for ordering of items
+            reverse (optional, bool): Should the items be reversed, default: False
+        """
+        key = functools.cmp_to_key(cmp) if cmp is not None else None
+        self._items = sorted(iterable, key=key, reverse=reverse)
+
+    def __getitem__(self, key):
+        return self._items[key]
+
+    def __len__(self):
+        return len(self._items)
+
+    def __repr__(self):
+        return '<%s %s>' % (self.__class__.__name__, self._items)
+
+
+# Dispatching structures
+
+class DispatchParam:
+    """Self-contained description of a single Dispatchable object's parameter.
+
+    Attributes:
+        name (str): Parameter name
+        type_ (type): Parameter type
+        default (optional, type): Parameter's default value, if any
+            (default: _MISSING, which is a sentinel meaning: No default is set)
+        **extras: Arbitrary extra info about the parameter
+    """
+    __slots__ = ['name', 'type_', 'default', 'extras']
+
+    def __init__(self, name: str, type_: type, default: type=_MISSING, **extras):
+        """DispatchParam constructor."""
+        self.name = name
+        self.type_ = type_
+        self.default = default
+        self.extras = extras
+
+    @classmethod
+    def from_signature(cls, signature: Signature, type_required: bool=False, **extras):
+        """Construct a DispatchParam from inspect.Signature object.
+
+        Args:
+            signature (Signature):
+            type_required (optional, bool):
+            **extras:
+        """
+        if type_required and signature.annotation is Signature.empty:
+            raise Exception('Untyped parameter')  # TODO: Specify which exception
+        default = signature.default if signature.default is not None else _MISSING
+        return cls(signature.name, signature.annotation, default, **extras)
+
+    def __repr__(self):
+        if self.default == _MISSING:
+            return '<%s %s: %s>' % (self.__class__.__name__, self.name, self.type_)
+        else:
+            return '<%s %s: %s = %s>' % (self.__class__.__name__, self.name,
+                                         self.type_, self.default)
+
+
+class Dispatchable(Callable):
+    """Callable wrapper containing info about all its parameters.
+
+    A Dispatchable object is a wrapper around any Python callable containing
+    info about all its parameters - their names, types, default values etc.
+
+    Attributes:
+        parameters (List[DispatchParam])
+        return_type (type)
+    """
+
+    __slots__ = ['_callable', 'parameters', 'return_type']
+
+    def __init__(self, callable: Callable):
+        if not isinstance(callable, Callable):
+            raise TypeError('Object is not a callable: %s' % callable)
+
+        self._callable = callable
+
+        call_sign = signature(callable)
+        self.parameters = {
+            k: DispatchParam.from_signature(v, type_required=True)
+                for k, v in call_sign.parameters.items()
+        }
+
+        self.return_type = call_sign.return_annotation
+
+    def __call__(self, *args, **kwargs):
+        return self._callable(args, kwargs)
+
+
+class ExceptionHandler(Dispatchable):
+    """"""
+
+    def __init__(self, exc_type, exc_handler, attrs):
+        pass
 
 
 # class SortedPriorityList:
@@ -196,83 +313,45 @@ class AttributeScope(collections.abc.MutableMapping):
 #     pass
 
 
-# import collections
+# Utility functions
 
-# class ImmutableDict(collections.Mapping):
-#     """Don't forget the docstrings!!"""
+def join_urls(*urls) -> str:
+    """Join multiple URL fragments together.
 
-#     def __init__(self, *args, **kwargs):
-#         self._d = dict(*args, **kwargs)
-#         self._hash = None
+    Args:
+        urls (List[str]): List of URL fragments to join
 
-#     def __iter__(self):
-#         return iter(self._d)
+    Returns:
+        str: URL joining result
+    """
+    if len(urls) == 0:
+        raise ValueError('No URLs to join')
 
-#     def __len__(self):
-#         return len(self._d)
+    leading = '\\' if urls[0][0] == '\\' else ''
+    trailing = '\\' if urls[-1][-1] == '\\' else ''
 
-#     def __getitem__(self, key):
-#         return self._d[key]
-
-#     def __hash__(self):
-#         # It would have been simpler and maybe more obvious to
-#         # use hash(tuple(sorted(self._d.iteritems()))) from this discussion
-#         # so far, but this solution is O(n). I don't know what kind of
-#         # n we are going to run into, but sometimes it's hard to resist the
-#         # urge to optimize when it will gain improved algorithmic performance.
-#         if self._hash is None:
-#             self._hash = 0
-#             for pair in self.iteritems():
-#                 self._hash ^= hash(pair)
-#         return self._hash
-
-# class CombinedDict:
-#     """
-#     Read-only combined dict which allows retrieval of values from any of the
-#     contained dicts, in order they were passed into the constructor.
-#     If the key isn't present, retrieval will return None.
-#     """
-
-#     def __init__(self, dicts):
-#         self.dicts = [d for d in dicts if d is not None]
-
-#     def __getitem__(self, key):
-#         for dct in self.dicts:
-#             if key in dct:
-#                 return dct[key]
-
-#     def __setitem__(self, key, value):
-#         raise TypeError('Cannot modify contents of a CombinedDict')
+    return leading + ('/'.join(u.strip('/') for u in urls)) + trailing
 
 
-# def url_join(*urls):
-#     """Join a list of URL fragments into a single URL"""
-#     if len(urls) == 0:
-#         raise ValueError('No URLs to join!')
-#     elif len(urls) == 1:
-#         return urls[0]
-#     else:
-#         return (urls[0].rstrip('/') + '/' +
-#                 '/'.join([u.strip('/') for u in urls[1:-1]]) +
-#                 '/' + urls[-1].lstrip('/'))
+def exc_type_cmp(exc_type1, exc_type2):
+    """Compare two exception types.
 
+    Standard comparison function returning -1, 0 or 1. Tests if one type is a
+    subclass of the other, then which one is more specific (lower in hierarchy),
+    and if still a tie, which has lexicographically smaller name.
+    """
 
-# def make_json(data_dict):
-#     """Return JSON representation of Python `data_dict` dictionary."""
-#     return json.dumps(data_dict, indent=4, sort_keys=True, separators=(',', ': '))
+    if issubclass(exc_type1, exc_type2):
+        return -1
+    elif issubclass(exc_type2, exc_type1):
+        return 1
 
+    def cmp(x, y):
+        """Inner utility compare function, standard format"""
+        return 0 if x == y else (-1 if x < y else 1)
 
-# # def exc_type_compare(exc_type1, exc_type2):
-# #     """Compare two exception types"""
-# #     if issubclass(exc_type1, exc_type2):
-# #         return -1
-# #     elif issubclass(exc_type2, exc_type1):
-# #         return 1
-# #     else:
-# #         sc_depth1 = len(exc_type1.__mro__)
-# #         sc_depth2 = len(exc_type2.__mro__)
+    # Comparison of exception types' depths in class hierarchy
+    #   (= number of superclasses of each)
+    dcmp = cmp(len(exc_type1.__mro__), len(exc_type2.__mro__))
 
-# #         if sc_depth1 == sc_depth2:
-# #             return -1 if exc_type1.__name__ < exc_type2.__name__ else 1
-# #         else:
-# #             return -1 if sc_depth1 < sc_depth2 else 1
+    return dcmp if dcmp != 0 else cmp(exc_type1.__name__, exc_type2.__name__)
